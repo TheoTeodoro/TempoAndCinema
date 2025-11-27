40831d5485ffbbffe06064347bb8ecf39eff16d6
Theo Teodoro <theotn12@gmail.com>
2025-11-26 21:05:11 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TempoAndCinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
