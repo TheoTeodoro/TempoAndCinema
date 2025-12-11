815fe6c840ff61a2d8b7ad3b8ecb72212c09e180
Alicyrc <alicyrc@hotmail.com>
2025-12-10 21:16:50 -0400

    Atualização e estilização de views de funcionalidades

---
+++ Dtos/TmdbReviewResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+public class TmdbReviewResponseDto
+{
+    [JsonPropertyName("results")]
+    public List<TmdbReviewDto> Results { get; set; } = new();
+}
+
+public class TmdbReviewDto
+{
+    [JsonPropertyName("author")]
+    public string Author { get; set; }
+
+    [JsonPropertyName("content")]
+    public string Content { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public string CreatedAt { get; set; }
+}
---
+++ Dtos/TmdbSimilarMoviesDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+public class TmdbSimilarMoviesDto
+{
+    [JsonPropertyName("results")]
+    public List<TmdbSimilarMovieDto> Results { get; set; } = new();
+}
+
+public class TmdbSimilarMovieDto
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("poster_path")]
+    public string PosterPath { get; set; }
+}
